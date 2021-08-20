10270b2adc97bfcc334e2b0b3d25031b2fd562ff
MateMarzec <64321779+MateMarzec@users.noreply.github.com>
2021-08-20 02:05:43 +0100

    Page load animation done

---
+++ assets/js/load.js
@@ -0,0 +1,13 @@
+
+  window.onload = (event) => {
+    var nav = document.querySelector('nav');
+    nav.classList.add('hidden');
+    setTimeout(function() {
+        var body = document.querySelector('body');
+        body.classList.add('loaded');
+    }, 1000);
+    setTimeout(function() {
+        nav.classList.remove('hidden');
+    }, 1800);
+    console.log('page is fully loaded');
+  };
